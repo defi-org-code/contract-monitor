c6554fb7be25277e79a5444647261a5f85d420a5
Yuval Aviguy <yuvala@Yuvals-MBP.lan>
2021-05-11 09:56:49 +0300

    remove try catch to monitor

--- src/main.js
+++ src/main.js
@@ -1,20 +1,10 @@
 let monitor = require('./monitor');
-var pastEvents = require('./pastEvents');
 
 ///////////////////////////////////
 const VERSION = "1.5"
 
 ///////////////////////////////////
 if (require.main === module) {
-  try{
-    let mon = new monitor.Monitor(VERSION);     
-    mon.start();      
-  }catch(e){
-    console.error("main error", e);
-    // send exception to discord
-    let str = tmplt_exception;
-    let estr = ""+e;
-    str = str.replace(/ERROR/g, estr);
-    sendAlert(str);
-  }
+  let mon = new monitor.Monitor(VERSION);     
+  mon.start();  
 }
