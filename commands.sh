89b06b7147f3446357b4754bd2c14aac413d84e9
Yuval Aviguy <yuvala@Yuvals-MBP.lan>
2021-05-17 19:07:22 +0300

    add graphite docker - no vol

--- commands.sh
+++ commands.sh
@@ -2,3 +2,29 @@
 #chmod +x setup.sh
 #pm2 start contract-monitor.sh --log-date-format 'DD-MM HH:mm:ss'
 #pm2 save
+
+
+# grafana is running on aviguy port 3000
+# graphite is running on docker
+# https://github.com/graphite-project/docker-graphite-statsd
+
+docker run -d --name graphite --restart=always -p 80:80 -p 2003-2004:2003-2004 -p 2023-2024:2023-2024 -p 8125:8125/udp -p 8126:8126  -v /opt/graphite/:/opt/graphite/  graphiteapp/graphite-statsd 
+
+
+docker run -d \
+--name graphite \
+--restart=always \
+-p 80:80 \
+-p 2003-2004:2003-2004 \
+-p 2023-2024:2023-2024 \
+-p 8125:8125/udp \
+-p 8126:8126 \
+-v /opt/graphite:/opt/graphite \
+-v /etc/graphite-statsd:/etc/graphite-statsd \
+graphiteapp/graphite-statsd
+
+  # mount vol never worked properly 
+
+#--name graphite
+# -v "/opt/graphite":"/opt/graphite" -v "/etc/graphite-statsd":"/etc/graphite-statsd"
+docker run -d --restart=always -p 80:80 -p 2003-2004:2003-2004 -p 2023-2024:2023-2024 -p 8125:8125/udp -p 8126:8126 graphiteapp/graphite-statsd
